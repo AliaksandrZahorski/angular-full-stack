c93d5218a9ac3ca74fa933fc48eb24d5f928a234
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2021-01-01 21:28:26 +0300

    init

---
+++ server/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const PORT = 4201;
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    console.log(`${req.ip} ${req.method} ${req.url}`);
+    next();
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send({ hello: 'world' });
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`server is running on port ${PORT}`);
+});
